4e60136b06598898a46c1934229c1219c1ebc56d
Adarsh Naik <adarshnayak108@gmail.com>
2026-08-28 23:10:26 +0530

    auth service basic function added without jwt

---
+++ app/services/auth_service.py
@@ -0,0 +1,25 @@
+from app.repositories.user_repository import UserRepository 
+from app.security.password import  hash_password, verify_password
+
+
+
+class AuthService:
+    def register_user(self, email:str , password:str):
+        usr_repo_object = UserRepository()
+        user_info =  usr_repo_object.get_user_by_email(email)
+        if user_info is not  None :
+            raise Exception("User with this email Already Exists")
+        
+        hashed_password = hash_password(password)
+
+        user_id = usr_repo_object.create_user(email,hashed_password)
+        return user_id
+
+        
+
+        
+
+        
+        
+        
+
--- test_user_repository.py
+++ test_user_repository.py
@@ -1,5 +1,5 @@
 from app.repositories.user_repository import UserRepository
-
+from app.services.auth_service import AuthService
 user  = UserRepository()
 #user_id=user.create_user("adarshnaik@mclaren.com","abc-123")
 
@@ -8,3 +8,7 @@ user_info=user.get_user_by_email("adarshnaik@mclaren.com")
 #print(user_id)
 print("-"*60)
 print(user_info)
+
+auth= AuthService()
+
+auth.register_user("adarshnaik@mclaren.com", "1234")
